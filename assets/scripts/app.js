607b3cd590fb6b8845bea6847275d6176b784448
rayhan-fardous <rayhan.rlp1@gmail.com>
2025-05-10 21:49:40 +0600

    Initial Upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+let currentResult = 0;
+
+currentResult = (currentResult + 10) * 2/1;
+
+let calculationDescription = '(' + currentResult + ' hi)';
+
+outputResult(currentResult, calculationDescription);
