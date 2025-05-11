382b93fe8a627d878c8ad01052c0e54d9e682ed9
rayhan-fardous <rayhan.rlp1@gmail.com>
2025-05-11 22:05:43 +0600

    learning

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,12 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 2/1;
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+currentResult = add(1,2);
 
 let calculationDescription = '(' + currentResult + ' hi)';
 
